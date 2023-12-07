9cfc22876424d084c1008443c0731983e8d26301
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-12-07 14:07:19 +0700

    chore: clean up command.php file
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- bin/command.php
+++ bin/command.php
@@ -1,7 +1,6 @@
 <?php
 
 use Fingerscan\Command;
-use Fingerscan\Device;
 
 set_time_limit(0);
 
@@ -10,10 +9,7 @@ $base_path = dirname(__DIR__);
 require($base_path.'/vendor/autoload.php');
 
 try {
-    $device = new Device('10.10.3.18');
-    $command = new Command($device);
-
-    $command->run($base_path.'/samples', ...array_slice($argv, 1));
+    (new Command())->run($base_path.'/samples', ...array_slice($argv, 1));
 
     exit(0);
 } catch (Throwable $e) {
